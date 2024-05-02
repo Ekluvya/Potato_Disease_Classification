c04ad7142fcc5f55c2d8567a63b6bd105cd8013e
Ekluvya <47681673+Ekluvya@users.noreply.github.com>
2024-05-02 22:54:05 +0530

    First commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import { ImageUpload } from '../components/home'
+
+function App() {
+
+
+  return (
+    <>
+    <div style={{backgroundColor:"green", height: "100vh"}}>
+     <ImageUpload/>
+     </div>
+     </>
+    
+  )
+}
+
+export default App
